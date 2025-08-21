69d335ce008a2c89bfaf298ee03777acdbcf10be
Callum <callumksx@gmail.com>
2025-08-20 21:10:52 -0400

    chore: add stub pages for /insights and /goals to back navbar links

---
+++ app/goals/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function GoalsPage() {
+  return <div className="p-6">Goals coming soon</div>
+}
+
---
+++ app/insights/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function InsightsPage() {
+  return <div className="p-6">Insights coming soon</div>
+}
+
